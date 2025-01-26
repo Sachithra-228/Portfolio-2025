90b895e0b4cd575b228e45cc4d90e16484d2a5f5
sachithra wijesinghe <wijesinghesachithra@gmail.com>
2025-01-26 10:55:55 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Simple form submission alert
+document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    alert('Thank you for reaching out! I will get back to you soon.');
+});
